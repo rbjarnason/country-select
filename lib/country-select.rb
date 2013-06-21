e3003932bd56637c0d9dda405304a0c055c81292
Róbert Viðar Bjarnason <robert.bjarnason@gmail.com>
2013-06-21 11:52:04 +0000

    Update country-select.rb

--- lib/country-select.rb
+++ lib/country-select.rb
@@ -52,7 +52,7 @@ module ActionView
         "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya",
         "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kosovo", "Kuwait", "Kyrgyzstan",
         "Lao People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
-        "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia, The Former Yugoslav Republic Of",
+        "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Republic of Macedonia",
         "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique",
         "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of",
         "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
